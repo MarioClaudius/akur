10bac6c9190ea3f38f68d26d3504ed324c1e85f8
Mario Claudius <mcmario7901@gmail.com>
2021-10-22 16:52:56 +0700

    add splash

---
+++ AkurAndroid/app/src/main/java/com/example/akurandroid/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.akurandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+    private ImageView splash;
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        splash = findViewById(R.id.splash);
+
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
