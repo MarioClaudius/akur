bca6de723292f3bce45be0ad69ec5aaa06d066e4
Mario Claudius <mcmario7901@gmail.com>
2021-10-30 17:24:53 +0700

    implement fragment in navigation view

--- AkurAndroid/app/src/main/java/com/example/akurandroid/HomeFragment.java
+++ AkurAndroid/app/src/main/java/com/example/akurandroid/HomeFragment.java
@@ -1,2 +1,27 @@
-package com.example.akurandroid;public class HomeFragment {
+package com.example.akurandroid;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+    private TextView tv;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        tv = v.findViewById(R.id.tv_home_fragment);
+        return v;
+    }
+
+    public void setText(){
+        tv.setText("TOMBOL DITEKAN");
+    }
 }
--- AkurAndroid/app/src/main/java/com/example/akurandroid/MainActivity.java
+++ AkurAndroid/app/src/main/java/com/example/akurandroid/MainActivity.java
@@ -1,21 +1,61 @@
 package com.example.akurandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener, View.OnClickListener {
     BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        floatingButton = findViewById(R.id.floating_button);
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);       //membuat background bottomNavigationView tidak ada
+        bottomNavigationView.setOnItemSelectedListener(this::onNavigationItemSelected);
+
+        floatingButton.setOnClickListener(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment selectedFragment = null;
+
+        switch(item.getItemId()){
+            case R.id.nav_home:
+                selectedFragment = new HomeFragment();
+                break;
+
+            case R.id.nav_account:
+                selectedFragment = new AccountFragment();
+                break;
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                selectedFragment).commit();
+
+        return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        FragmentManager fm = getSupportFragmentManager();
+        HomeFragment fragment = (HomeFragment) fm.findFragmentById(R.id.fragment_container);
+        fragment.setText();
+    }
 }
