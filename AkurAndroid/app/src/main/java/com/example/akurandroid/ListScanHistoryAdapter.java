40f77b4c47ce4b5ca6b29f2a0b3e5ce6e036a0de
Mario Claudius <mcmario7901@gmail.com>
2021-11-24 10:06:29 +0700

    dialog box for packet's information in history done

--- AkurAndroid/app/src/main/java/com/example/akurandroid/ListScanHistoryAdapter.java
+++ AkurAndroid/app/src/main/java/com/example/akurandroid/ListScanHistoryAdapter.java
@@ -1,13 +1,19 @@
 package com.example.akurandroid;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.button.MaterialButton;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -18,6 +24,7 @@ import java.util.Locale;
 
 public class ListScanHistoryAdapter extends RecyclerView.Adapter<ListScanHistoryAdapter.ListViewHolder> {
     private List<ScanHistory> listHistory;
+    private Dialog dialog;
 
     public ListScanHistoryAdapter(List<ScanHistory> list){
         this.listHistory = list;
@@ -48,6 +55,30 @@ public class ListScanHistoryAdapter extends RecyclerView.Adapter<ListScanHistory
         String yearHour = FORMAT_YEAR_HOUR.format(date);
         holder.tvDayMonth.setText(dayMonth);
         holder.tvYearHour.setText(yearHour);
+        holder.btnHistory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog = new Dialog(v.getContext());
+                dialog.setContentView(R.layout.dialog_packet_information);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                TextView tvIdScan = dialog.findViewById(R.id.tv_id_qr_information);
+                TextView tvNumberReceipt = dialog.findViewById(R.id.tv_number_receipt_information);
+                TextView tvShipmentName = dialog.findViewById(R.id.tv_shipment_name_information);
+                TextView tvShipmentType = dialog.findViewById(R.id.tv_shipment_type_information);
+                MaterialButton doneBtn = dialog.findViewById(R.id.btn_done_information);
+                tvIdScan.setText("#" + scanHistory.getId());
+                tvNumberReceipt.setText(scanHistory.getReceiptNumber());
+                tvShipmentName.setText(scanHistory.getCourierName());
+                tvShipmentType.setText(scanHistory.getCourierType());
+                dialog.show();
+                doneBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+            }
+        });
     }
 
     @Override
@@ -57,6 +88,7 @@ public class ListScanHistoryAdapter extends RecyclerView.Adapter<ListScanHistory
 
     public class ListViewHolder extends RecyclerView.ViewHolder{
         TextView tvReceiptNumber, tvShipmentType, tvDayMonth, tvYearHour;
+        Button btnHistory;
 
         public ListViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -64,6 +96,7 @@ public class ListScanHistoryAdapter extends RecyclerView.Adapter<ListScanHistory
             tvShipmentType = itemView.findViewById(R.id.tv_shipment_type_history);
             tvDayMonth = itemView.findViewById(R.id.tv_day_month);
             tvYearHour = itemView.findViewById(R.id.tv_year_hour);
+            btnHistory = itemView.findViewById(R.id.btn_history);
         }
     }
 }
